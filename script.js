3903bc61364b60425a4069ad23e39e82f44687ae
saidlhbal520-png <saidlhbal520@gmail.com>
2026-03-17 22:43:34 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
 // Splash
 setTimeout(() => {
   document.getElementById("splash").style.display = "none";
-  document.getElementById("main").classList.remove("hidden");
+  document.getElementById("app").classList.remove("hidden");
 }, 3000);
 
 // Navigation
-function showSection(id) {
-  let sections = document.querySelectorAll(".section");
-  sections.forEach(sec => sec.classList.add("hidden"));
+function openScreen(id) {
+  document.querySelectorAll(".screen").forEach(s => s.classList.remove("active"));
+  document.getElementById(id).classList.add("active");
 
-  document.getElementById(id).classList.remove("hidden");
+  if (id === "map") {
+    loadMap();
+  }
+}
+
+// Google Map
+function loadMap() {
+  const map = new google.maps.Map(document.getElementById("mapContainer"), {
+    center: { lat: 29.697, lng: -9.731 }, // Tiznit
+    zoom: 14
+  });
+
+  new google.maps.Marker({
+    position: { lat: 29.697, lng: -9.731 },
+    map: map,
+    title: "صيدلية"
+  });
 }
